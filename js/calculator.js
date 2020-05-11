4a630b1caa376bcbdcee63f5d2895965a46d4038
Austin Hulen <ahulen08@gmail.com>
2020-05-11 16:12:30 -0700

    Add operate function

--- js/calculator.js
+++ js/calculator.js
@@ -13,3 +13,23 @@ function multiply(num1, num2) {
 function divide (num1, num2) {
     return num1 / num2; 
 }
+
+function operate (operator, a, b) {
+    switch (operator) {
+        case '+': 
+            add(a, b);
+            break;
+        case '-': 
+            subtract(a, b);
+            break;
+        case '*':
+            multiply(a, b);
+            break;
+        case '/': 
+            divide(a,b);
+            break;
+        default: 
+            'Error, command not recongnized.'
+            break;
+    }
+}
